0bb0a7a8d0c3a86c07f073345866b626b42e8c05
ned66 <ned66@DENIS>
2025-01-25 13:37:58 +0500

    struct Student void initStudent double averMark void printStudent

--- les01_01.cpp
+++ les01_01.cpp
@@ -1,20 +1,81 @@
-﻿// les01_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// les01_01.cpp 
+// Вычислить среднюю оценку студента
 
 #include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+// максимальная длина имени
+const int maxNameLength{ 20 };
+
+// число оценок
+const int markCount{ 3 };
+
+// определение структуры данных
+// ---------------------------------------
+// Студент
+struct Student
+{
+    // ФИО
+    char name[maxNameLength];
+    // оценки
+    int marks[markCount];
+};
+// ---------------------------------------
+// конец определения структуры
+
+// функция обработки структуры данных
+// ---------------------------------------
+// заполнение данных о студенте
+void initStudent(Student &student, const char *name, const int marks[])
+{
+    strcpy_s(student.name, maxNameLength, name);
+    for (int i = 0; i < markCount; i++)
+    {
+        student.marks[i] = marks[i];
+    }
+}
+
+// вычисление средней оценки
+double averMark(Student student)
+{
+    double sum{ 0 };
+    for (int i = 0; i < markCount; i++)
+    {
+        sum += student.marks[i];
+    }
+    return sum / markCount;
+}
+
+// вывод данных о студенте
+void printStudent(Student student)
+{
+    cout << student.name << endl;
+    cout << "Оценки: ";
+    for (int i = 0; i < markCount; i++)
+    {
+        cout << setw(4) << student.marks[i];
+    }
+    cout << endl;
+}
+// конец списка функций обработки структуры данных
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "");
+
+    cout << "Успеваемость студента." << endl << endl;
+    Student student;
+    const char *studentName{ "Петров А.О." };
+    int studentMarks[]{ 4, 4, 3 };
+
+    initStudent(student, studentName, studentMarks);
+    printStudent(student);
+    cout << "Средняя оценка: " << fixed << setprecision(2) << averMark(student) << endl;
+
+    _getch();
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
